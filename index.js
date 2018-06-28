69aa1c326720844aa96e29dc61432ef961bc3134
Ramon G Victor <kazefiro@MacBook-Pro-de-Ramon.local>
2018-06-28 19:52:26 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 
-AppRegistry.registerComponent('ToDoManager', () => App);
+AppRegistry.registerComponent('ToDoManager', () => Register);
